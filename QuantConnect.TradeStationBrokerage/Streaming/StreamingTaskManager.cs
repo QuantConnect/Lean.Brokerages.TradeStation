9189094e18db46387326261b358a314cfeaf4ff1
Romazes <romazes20@gmail.com>
2024-10-16 18:39:54 +0300

    refactor: multiple subscription process feat: StreamTaskManager with stream functionally feat: TradeStation Multiple Subscription Manager to subscribe on multiple symbols remove: handle response in DQH partial class

---
+++ QuantConnect.TradeStationBrokerage/Streaming/StreamingTaskManager.cs
@@ -0,0 +1,221 @@
+﻿/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Linq;
+using System.Threading;
+using QuantConnect.Logging;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace QuantConnect.Brokerages.TradeStation.Streaming;
+
+/// <summary>
+/// Manages streaming tasks for a collection of items, allowing for subscription, unSubscription and restarting of streaming processes.
+/// </summary>
+public class StreamingTaskManager
+{
+    /// <summary>
+    /// Indicates whether there are any pending subscription processes.
+    /// </summary>
+    private bool _hasPendingSubscriptions;
+
+    /// <summary>
+    /// Signals to a <see cref="CancellationToken"/> that it should be canceled.
+    /// </summary>
+    private CancellationTokenSource _cancellationTokenSource = new();
+
+    /// <summary>
+    /// The task representing the ongoing streaming operation.
+    /// </summary>
+    private Task _streamingTask;
+
+    /// <summary>
+    /// The maximum number of items that can be subscribed to.
+    /// </summary>
+    private readonly int _maxSubscriptionLimit;
+
+    /// <summary>
+    /// Synchronization object used to ensure thread safety when starting or restarting the streaming task.
+    /// </summary>
+    private readonly object _streamingTaskLock = new();
+
+    /// <summary>
+    /// Specifies the delay interval between subscription attempts.
+    /// </summary>
+    private readonly TimeSpan _subscribeDelay = TimeSpan.FromMilliseconds(1000);
+
+    /// <summary>
+    /// The action to execute for streaming the subscribed items.
+    /// </summary>
+    private readonly Func<IReadOnlyCollection<string>, CancellationToken, Task<bool>> _streamAction;
+
+    /// <summary>
+    /// Event used to signal the completion of the streaming task.
+    /// </summary>
+    private readonly AutoResetEvent autoResetEvent = new(false);
+
+    /// <summary>
+    /// Gets the collection of subscribed items.
+    /// </summary>
+    public readonly HashSet<string> subscriptionBrokerageTickers;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="StreamingTaskManager"/> class.
+    /// </summary>
+    /// <param name="streamingAction">The action to execute for streaming the items.</param>
+    /// <param name="initialSubscribedItems">Initial collection of items to subscribe to.</param>
+    /// <param name="maxSubscriptionLimit">The maximum number of items that can be subscribed to. (Defaults to 100)</param>
+    public StreamingTaskManager(
+        Func<IReadOnlyCollection<string>, CancellationToken, Task<bool>> streamingAction,
+        IEnumerable<string> initialSubscribedItems,
+        int maxSubscriptionLimit = 100)
+    {
+        _streamAction = streamingAction ?? throw new ArgumentNullException(nameof(streamingAction), "Streaming action cannot be null.");
+        subscriptionBrokerageTickers = new(initialSubscribedItems ?? throw new ArgumentNullException(nameof(initialSubscribedItems), "Initial subscribed items cannot be null."));
+        _maxSubscriptionLimit = maxSubscriptionLimit;
+    }
+
+    /// <summary>
+    /// Starts the streaming task and executes the provided streaming action.
+    /// </summary>
+    public void StartStreaming()
+    {
+        lock (_streamingTaskLock)
+        {
+            if (_hasPendingSubscriptions)
+            {
+                // Avoid duplicate subscriptions by checking if a subscription is already in progress
+                return;
+            }
+            _hasPendingSubscriptions = true;
+        }
+
+        _streamingTask = Task.Factory.StartNew(async () =>
+        {
+            // Wait for a specified delay to batch multiple symbol subscriptions into a single request
+            await Task.Delay(_subscribeDelay).ConfigureAwait(false);
+
+            List<string> brokerageTickers;
+            lock (_streamingTaskLock)
+            {
+                _hasPendingSubscriptions = false;
+                brokerageTickers = subscriptionBrokerageTickers.ToList();
+                if (brokerageTickers.Count == 0)
+                {
+                    // If there are no symbols to subscribe to, exit the task
+                    Log.Trace($"{nameof(StreamingTaskManager)}.{nameof(StartStreaming)}: No symbols to subscribe to at this time. Exiting subscription task.");
+                    return;
+                }
+            }
+
+            try
+            {
+                var result = await _streamAction(brokerageTickers, _cancellationTokenSource.Token);
+            }
+            catch (Exception ex) when (ex is not OperationCanceledException)
+            {
+                Log.Error($"{nameof(StreamingTaskManager)}.Exception: {ex}");
+            }
+            finally
+            {
+                // Signal: task is completed
+                autoResetEvent.Set();
+            }
+        });
+    }
+
+    /// <summary>
+    /// Stops the currently running streaming task and cancels the current task.
+    /// </summary>
+    public void StopStreaming()
+    {
+        Log.Debug($"{nameof(StreamingTaskManager)}.{nameof(StopStreaming)}: Stopping the current streaming task.");
+
+        if (_streamingTask != null)
+        {
+            _cancellationTokenSource.Cancel();
+
+            if (!autoResetEvent.WaitOne(TimeSpan.FromSeconds(5)))
+            {
+                Log.Error($"{nameof(StreamingTaskManager)}.{nameof(StopStreaming)}: Timeout while waiting for the streaming task to complete.");
+            }
+
+            try
+            {
+                _streamingTask.Wait();
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"{nameof(StreamingTaskManager)}.{nameof(StopStreaming)}: Error during task cancellation: {ex}");
+            }
+            finally
+            {
+                _cancellationTokenSource.Dispose();
+                _cancellationTokenSource = new CancellationTokenSource();
+            }
+        }
+    }
+
+    /// <summary>
+    /// Restarts the streaming task by stopping the current one and starting a new one. 
+    /// This is useful for updating subscriptions without needing to manually stop and start.
+    /// </summary>
+    public void RestartStreaming()
+    {
+        StopStreaming();
+        StartStreaming();
+    }
+
+    /// <summary>
+    /// Adds an item to the subscription list if the maximum limit is not reached. 
+    /// If the item is already present, it will not be added, and the method will return false.
+    /// </summary>
+    /// <param name="item">The item to add to the subscription list. This should be a unique identifier 
+    /// for the item being subscribed to.</param>
+    /// <returns><c>true</c> if the item was added successfully; otherwise, <c>false</c>.</returns>
+    public bool AddSubscriptionItem(string item)
+    {
+        if (subscriptionBrokerageTickers.Count >= _maxSubscriptionLimit)
+        {
+            Log.Debug($"{nameof(StreamingTaskManager)}.{nameof(AddSubscriptionItem)}: Cannot add more items. Maximum limit reached.");
+            return false;
+        }
+
+        if (!subscriptionBrokerageTickers.Add(item))
+        {
+            Log.Debug($"{nameof(StreamingTaskManager)}.{nameof(AddSubscriptionItem)}: Item already exists in the list.");
+            return false;
+        }
+
+        return true;
+    }
+
+    /// <summary>
+    /// Removes an item from the subscription list.
+    /// </summary>
+    /// <param name="item">The item to remove from the subscription list.</param>
+    /// <returns><c>true</c> if the item was removed successfully; otherwise, <c>false</c>.</returns>
+    public bool RemoveSubscriptionItem(string item)
+    {
+        if (subscriptionBrokerageTickers.Remove(item))
+        {
+            return true;
+        }
+
+        Log.Debug($"{nameof(StreamingTaskManager)}.{nameof(RemoveSubscriptionItem)}: Cannot remove item: [{item}]. Item not found.");
+        return false;
+    }
+}
---
+++ QuantConnect.TradeStationBrokerage/TradeStationBrokerageMultiStreamSubscriptionManager.cs
@@ -0,0 +1,344 @@
+﻿/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using NodaTime;
+using System.Linq;
+using QuantConnect.Data;
+using QuantConnect.Logging;
+using QuantConnect.Data.Market;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using QuantConnect.Brokerages.TradeStation.Api;
+using QuantConnect.Brokerages.TradeStation.Models;
+using QuantConnect.Brokerages.TradeStation.Streaming;
+
+namespace QuantConnect.Brokerages.TradeStation
+{
+    /// <summary>
+    /// Manages multiple streaming subscriptions for the TradeStation brokerage, handling updates received from the TradeStation API.
+    /// </summary>
+    public class TradeStationBrokerageMultiStreamSubscriptionManager : EventBasedDataQueueHandlerSubscriptionManager, IDisposable
+    {
+        /// <summary>
+        /// The maximum number of symbols allowed per quote stream request.
+        /// </summary>
+        /// <see href="https://api.tradestation.com/docs/specification#tag/MarketData/operation/GetQuoteChangeStream"/>
+        private const int MaxSymbolsPerQuoteStreamRequest = 100;
+
+        /// <summary>
+        /// Manages the list of active quote stream managers.
+        /// </summary>
+        private List<StreamingTaskManager> _quoteStreamManagers = new();
+
+        /// <summary>
+        /// A thread-safe dictionary that stores the order books by brokerage symbols.
+        /// </summary>
+        private readonly ConcurrentDictionary<string, DefaultOrderBook> _orderBooks = new();
+
+        /// <summary>
+        /// TradeStation api client implementation
+        /// </summary>
+        private TradeStationApiClient _tradeStationApiClient;
+
+        /// <summary>
+        /// Use like synchronization context for threads
+        /// </summary>
+        private readonly object _synchronizationContext = new();
+
+        /// <summary>
+        /// Aggregates ticks and bars based on given subscriptions.
+        /// </summary>
+        protected IDataAggregator _aggregator;
+
+        /// <summary>
+        /// Provides the mapping between Lean symbols and brokerage specific symbols.
+        /// </summary>
+        private TradeStationSymbolMapper _symbolMapper;
+
+        /// <summary>
+        /// A thread-safe dictionary that maps a <see cref="Symbol"/> to a <see cref="DateTimeZone"/>.
+        /// </summary>
+        /// <remarks>
+        /// This dictionary is used to store the time zone information for each symbol in a concurrent environment,
+        /// ensuring thread safety when accessing or modifying the time zone data.
+        /// </remarks>
+        private readonly ConcurrentDictionary<Symbol, DateTimeZone> _exchangeTimeZoneByLeanSymbol = new();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="tradeStationApiClient"></param>
+        /// <param name="symbolMapper"></param>
+        /// <param name="aggregator"></param>
+        public TradeStationBrokerageMultiStreamSubscriptionManager(TradeStationApiClient tradeStationApiClient, TradeStationSymbolMapper symbolMapper, IDataAggregator aggregator)
+        {
+            _aggregator = aggregator;
+            _symbolMapper = symbolMapper;
+            _tradeStationApiClient = tradeStationApiClient;
+
+            SubscribeImpl = (symbols, _) => Subscribe(symbols);
+            UnsubscribeImpl = (symbols, _) => UnSubscribe(symbols);
+        }
+
+        private bool Subscribe(IEnumerable<Symbol> symbols)
+        {
+            var subscribedSymbols = new List<string>();
+            foreach (var symbol in symbols)
+            {
+                subscribedSymbols.Add(AddOrderBook(symbol));
+            }
+
+            foreach (var quoteStream in _quoteStreamManagers)
+            {
+                if (subscribedSymbols.Count == 0)
+                {
+                    break;
+                }
+
+                var symbolAdded = default(bool);
+
+                foreach (var symbol in subscribedSymbols)
+                {
+                    if (quoteStream.AddSubscriptionItem(symbol))
+                    {
+                        subscribedSymbols.Remove(symbol);
+                        symbolAdded = true;
+                    }
+
+                    if (subscribedSymbols.Count == 0)
+                    {
+                        break;
+                    }
+                }
+
+                if (symbolAdded)
+                {
+                    quoteStream.RestartStreaming();
+                }
+            }
+
+            if (subscribedSymbols.Count > 0)
+            {
+                var brokerageSymbolsChunks = subscribedSymbols.Chunk(MaxSymbolsPerQuoteStreamRequest);
+
+                foreach (var brokerageSymbolChunk in brokerageSymbolsChunks)
+                {
+                    var streamQuoteTask = new StreamingTaskManager(async (brokerageTickers, cancellationToken) =>
+                    {
+                        await foreach (var quote in _tradeStationApiClient.StreamQuotes(brokerageTickers, cancellationToken))
+                        {
+                            HandleQuoteEvents(quote);
+                        }
+
+                        return false;
+
+                    }, brokerageSymbolChunk, MaxSymbolsPerQuoteStreamRequest);
+
+                    _quoteStreamManagers.Add(streamQuoteTask);
+
+                    streamQuoteTask.StartStreaming();
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Unsubscribes from updates for the specified collection of symbols.
+        /// </summary>
+        /// <param name="symbols">A collection of symbols to unsubscribe from.</param>
+        /// <returns>Always, Returns <c>true</c> if the unSubscription was successful</returns>
+        private bool UnSubscribe(IEnumerable<Symbol> symbols)
+        {
+            var streamsToRemove = new List<StreamingTaskManager>();
+
+            foreach (var symbol in symbols)
+            {
+                foreach (var streamQuoteTask in _quoteStreamManagers)
+                {
+                    if (streamQuoteTask.RemoveSubscriptionItem(RemoveOrderBook(symbol)))
+                    {
+                        streamQuoteTask.RestartStreaming();
+                    }
+
+                    if (streamQuoteTask.subscriptionBrokerageTickers.Count == 0)
+                    {
+                        streamsToRemove.Add(streamQuoteTask);
+                        _quoteStreamManagers.Remove(streamQuoteTask);
+                    }
+
+                    if (_quoteStreamManagers.Count == 0)
+                    {
+                        break;
+                    }
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Handles incoming quote events and updates the order books accordingly.
+        /// </summary>
+        /// <param name="quote">The incoming quote containing bid, ask, and trade information.</param>
+        private void HandleQuoteEvents(Quote quote)
+        {
+            if (_orderBooks.TryGetValue(quote.Symbol, out var orderBook))
+            {
+                if (quote.Ask > 0 && quote.AskSize > 0)
+                {
+                    orderBook.UpdateAskRow(quote.Ask, quote.AskSize);
+                }
+                else if (quote.AskSize == 0 && quote.Ask != 0)
+                {
+                    orderBook.RemoveAskRow(quote.Ask);
+                }
+
+                if (quote.Bid > 0 && quote.BidSize > 0)
+                {
+                    orderBook.UpdateBidRow(quote.Bid, quote.BidSize);
+                }
+                else if (quote.BidSize == 0 && quote.Bid != 0)
+                {
+                    orderBook.RemoveBidRow(quote.Bid);
+                }
+
+                if (quote.Last > 0 && quote.LastSize > 0)
+                {
+                    EmitTradeTick(orderBook.Symbol, quote.Last, quote.LastSize, quote.TradeTime);
+                }
+            }
+            else
+            {
+                Log.Error($"{nameof(TradeStationBrokerage)}.{nameof(HandleQuoteEvents)}: Symbol {quote.Symbol} not found in order books. This could indicate an unexpected symbol or a missing initialization step.");
+            }
+        }
+
+        /// <summary>
+        /// Emits a trade tick with the provided details and updates the aggregator.
+        /// </summary>
+        /// <param name="symbol">The symbol of the traded instrument.</param>
+        /// <param name="price">The trade price.</param>
+        /// <param name="size">The trade size.</param>
+        /// <param name="tradeTime">The time of the trade.</param>
+        private void EmitTradeTick(Symbol symbol, decimal price, decimal size, DateTime tradeTime)
+        {
+            if (!_exchangeTimeZoneByLeanSymbol.TryGetValue(symbol, out var exchangeTimeZone))
+            {
+                return;
+            }
+
+            var tradeTick = new Tick
+            {
+                Value = price,
+                Time = DateTime.UtcNow.ConvertFromUtc(exchangeTimeZone),
+                Symbol = symbol,
+                TickType = TickType.Trade,
+                Quantity = size
+            };
+
+            lock (_synchronizationContext)
+            {
+                _aggregator.Update(tradeTick);
+            }
+        }
+
+        /// <summary>
+        /// Handles updates to the best bid and ask prices and updates the aggregator with a new quote tick.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="bestBidAskUpdatedEvent">The event arguments containing best bid and ask details.</param>
+        private void OnBestBidAskUpdated(object sender, BestBidAskUpdatedEventArgs bestBidAskUpdatedEvent)
+        {
+            if (!_exchangeTimeZoneByLeanSymbol.TryGetValue(bestBidAskUpdatedEvent.Symbol, out var exchangeTimeZone))
+            {
+                return;
+            }
+
+            var tick = new Tick
+            {
+                AskPrice = bestBidAskUpdatedEvent.BestAskPrice,
+                BidPrice = bestBidAskUpdatedEvent.BestBidPrice,
+                Time = DateTime.UtcNow.ConvertFromUtc(exchangeTimeZone),
+                Symbol = bestBidAskUpdatedEvent.Symbol,
+                TickType = TickType.Quote,
+                AskSize = bestBidAskUpdatedEvent.BestAskSize,
+                BidSize = bestBidAskUpdatedEvent.BestBidSize
+            };
+            tick.SetValue();
+
+            lock (_synchronizationContext)
+            {
+                _aggregator.Update(tick);
+            }
+        }
+
+        /// <summary>
+        /// Adds an order book for the specified symbol if it does not already exist.
+        /// </summary>
+        /// <param name="symbol">The symbol for which the order book is to be added.</param>
+        private string AddOrderBook(Symbol symbol)
+        {
+            var exchangeTimeZone = symbol.GetSymbolExchangeTimeZone();
+            _exchangeTimeZoneByLeanSymbol[symbol] = exchangeTimeZone;
+
+            var brokerageSymbol = _symbolMapper.GetBrokerageSymbol(symbol);
+
+            if (!_orderBooks.TryGetValue(brokerageSymbol, out var orderBook))
+            {
+                _orderBooks[brokerageSymbol] = new DefaultOrderBook(symbol);
+                _orderBooks[brokerageSymbol].BestBidAskUpdated += OnBestBidAskUpdated;
+            }
+
+            return brokerageSymbol;
+        }
+
+        /// <summary>
+        /// Removes the order book for the specified symbol if it exists.
+        /// </summary>
+        /// <param name="symbol">The symbol for which the order book is to be removed.</param>
+        private string RemoveOrderBook(Symbol symbol)
+        {
+            _exchangeTimeZoneByLeanSymbol.Remove(symbol, out _);
+
+            var brokerageSymbol = _symbolMapper.GetBrokerageSymbol(symbol);
+
+            if (_orderBooks.TryRemove(brokerageSymbol, out var orderBook))
+            {
+                orderBook.BestBidAskUpdated -= OnBestBidAskUpdated;
+            }
+
+            return brokerageSymbol;
+        }
+
+        public override void Dispose()
+        {
+            if (_quoteStreamManagers != null)
+            {
+                // Stop each stream in the manager
+                foreach (var streamQuote in _quoteStreamManagers)
+                {
+                    streamQuote.StopStreaming();
+                }
+
+                // Clear the list to release resources
+                _quoteStreamManagers.Clear();
+                _quoteStreamManagers = null;
+            }
+
+
+        }
+    }
+}
