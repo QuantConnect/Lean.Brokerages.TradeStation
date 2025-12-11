63a783019f2728eb7a7e5e5bee49bb5c5d74d1fe
Romazes <romazes20@gmail.com>
2025-12-11 18:56:00 +0200

    feat: develop HttpClientRetryWrapper

---
+++ QuantConnect.TradeStationBrokerage.Tests/TradeStationBrokerageHttpClientRetryWrapperTests.cs
@@ -0,0 +1,116 @@
+﻿/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+
+using System;
+using System.Net;
+using NUnit.Framework;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using QuantConnect.Brokerages.TradeStation.Api;
+
+namespace QuantConnect.Brokerages.TradeStation.Tests;
+
+[TestFixture]
+public class TradeStationBrokerageHttpClientRetryWrapperTests
+{
+    private readonly string _baseUrl = "https://api.test/";
+
+    [Test]
+    public async Task SendAsync_SuccessfulResponse_ReturnsResponse()
+    {
+        var handler = new TestHttpMessageHandler((req, ct) =>
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent("ok")
+            };
+            return Task.FromResult(response);
+        });
+
+        using var wrapper = new HttpClientRetryWrapper(_baseUrl, handler, 5, TimeSpan.FromSeconds(10), TimeSpan.FromSeconds(2));
+
+        var response = await wrapper.SendAsync("/resource", HttpMethod.Get, externalCancellationToken: CancellationToken.None);
+
+        Assert.IsNotNull(response);
+        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+    }
+
+    [Test]
+    public void SendAsyncCallerCancellationThrowsTaskCanceledException()
+    {
+        var handler = new TestHttpMessageHandler((req, ct) =>
+        {
+            ct.ThrowIfCancellationRequested();
+            return Task.FromResult(new HttpResponseMessage(HttpStatusCode.OK));
+        });
+
+        using var wrapper = new HttpClientRetryWrapper(
+            _baseUrl,
+            handler,
+            maxRetries: 1,
+            ctsAttemptTimeout: TimeSpan.FromSeconds(1),
+            backOffDelay: TimeSpan.Zero);
+
+        using var cts = new CancellationTokenSource();
+        cts.Cancel();
+
+        Assert.ThrowsAsync<TaskCanceledException>(async () =>
+        {
+            await wrapper.SendAsync("/resource", HttpMethod.Get, externalCancellationToken: cts.Token);
+        });
+    }
+
+    [Test]
+    public void SendAsyncAttemptTimeoutsRetriesAndThrowsWhenMaxRetriesReached()
+    {
+        var handler = new TestHttpMessageHandler((req, ct) =>
+        {
+            return Task.FromResult(new HttpResponseMessage(HttpStatusCode.OK));
+        });
+
+        using var wrapper = new HttpClientRetryWrapper(
+            _baseUrl,
+            handler,
+            maxRetries: 2,
+            ctsAttemptTimeout: TimeSpan.Zero,
+            backOffDelay: TimeSpan.Zero);
+
+        Assert.ThrowsAsync<TaskCanceledException>(async () =>
+        {
+            await wrapper.SendAsync("/resource", HttpMethod.Get, externalCancellationToken: CancellationToken.None);
+        });
+    }
+}
+
+/// <summary>
+/// Minimal HttpMessageHandler used in tests that returns a preconfigured response
+/// or observes the incoming cancellation token.
+/// </summary>
+public class TestHttpMessageHandler : HttpMessageHandler
+{
+    private readonly Func<HttpRequestMessage, CancellationToken, Task<HttpResponseMessage>> _sendAsync;
+
+    public TestHttpMessageHandler(Func<HttpRequestMessage, CancellationToken, Task<HttpResponseMessage>> sendAsync)
+    {
+        _sendAsync = sendAsync;
+    }
+
+    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        return _sendAsync(request, cancellationToken);
+    }
+}
